58aa320c132461911ba48fa1fb9595b78f599cfa
Margarida Cautela <maria.cautela@readinessit.com>
2019-12-10 19:07:34 +0000

    #first commit

---
+++ src/EAIAutomation.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class EAIAutomation {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.navigate().to("https://ola.pt");
+
+        driver.close();
+    }
+}
